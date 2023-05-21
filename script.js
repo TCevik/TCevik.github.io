6513c2c97c09586c626963c69822e9908f6c6c88
Tamer Çevik <133103549+TCevik@users.noreply.github.com>
2023-05-21 11:04:53 +0200

    Update

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var currentDate = new Date();
+    var targetDate = new Date(currentDate.getFullYear(), 4, 30); // 4 staat voor mei (index begint bij 0)
 
+    if (currentDate >= targetDate) {
+       var element = document.getElementById("birthday-message");
+       element.style.display = "none";
+    }
+});
