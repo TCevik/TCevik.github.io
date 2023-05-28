0ae33a7adb9c77144909e7b287f35117a3928cbc
Tamer Çevik <tam.cevik123@gmail.com>
2023-05-28 10:27:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -115,3 +115,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
   bookIcon.addEventListener('click', readPage);
 });
+
+
+
+
+// Controleer of de UserName in localStorage overeenkomt met de vereiste waarde
+if (localStorage.getItem('UserName') === 'SercanBroertjeVanTamer') {
+  // Maak een nieuw h1-element aan
+  var bannedMessage = document.createElement('h1');
+  bannedMessage.textContent = 'Je bent verbannen';
+
+  // Leeg de body van de pagina
+  document.body.innerHTML = '';
+
+  // Voeg het h1-element toe aan de body van de pagina
+  document.body.appendChild(bannedMessage);
+}
